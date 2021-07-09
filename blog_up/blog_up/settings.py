ae3266c13b08b50427fe14940dbc531df1057af6
mehedi432 <aamehedi93@gmail.com>
2021-07-09 08:35:03 +0000

    Added footer

--- blog_up/blog_up/settings.py
+++ blog_up/blog_up/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)@4u5+q1o5c((4ajpg3a$3dekpke(71)l2^k#43knnew)4gcw-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mehediabdullah.pythonanywhere.com']
 
 
 # Application definition
@@ -112,7 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/home/mehediabdullah/lovely_project/blog_up/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
